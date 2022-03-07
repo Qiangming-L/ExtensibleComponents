67ae84639778e2c6d78c23330088dcecad4f1de1
Abel <1043477154@qq.com>
2022-03-07 17:37:43 +0800

    add module

--- data.js
+++ data.js
@@ -1,4 +1,13 @@
-const components = ["ButtonAnimation", "CompositionInput", "DatePicker"];
+const components = [
+  "ButtonAnimation",
+  "CompositionInput",
+  "DatePicker",
+  "LazyLoad",
+  "MagnifyingGlass",
+  "MagnifyingView",
+  "MasonryLayouts",
+  "TimePicker",
+];
 const dataArr = {
   ButtonAnimation: {
     title: "ButtonAnimation",
@@ -13,7 +22,10 @@ const dataArr = {
     ],
     codeSandboxReact:
       "https://codesandbox.io/s/stoic-surf-08qnte?file=/src/App.tsx",
+    githubReact:
+      "https://github.com/Qiangming-L/Extensible-Components/tree/main/components/ButtonAnimation/react",
     codeSandboxVue: "",
+    githubVue: "",
   },
   CompositionInput: {
     title: "CompositionInput",
@@ -64,11 +76,16 @@ const dataArr = {
       },
     ],
     codeSandboxReact: "https://codesandbox.io/s/charming-night-9q8enl",
+    githubReact:
+      "https://github.com/Qiangming-L/Extensible-Components/tree/main/components/CompositionInput/react",
     codeSandboxVue: "",
+    githubVue: "",
   },
   DatePicker: {
     title: "DatePicker",
     text: "日期选择框",
+    reference:
+      "引用 <a class='content-reference' target='view_window' href='https://github.com/Qiangming-L/Extensible-Components/blob/main/mixins/timestamp.tsx'>mixins/timestamp</a>",
     tbody: [
       {
         propsName: "[className]",
@@ -90,7 +107,7 @@ const dataArr = {
       },
       {
         propsName: "[selectionDate]",
-        instructions: "默认选中日期",
+        instructions: "弹出层显示日期",
         type: "Date",
         default: "-",
       },
@@ -161,16 +178,21 @@ const dataArr = {
         default: "-",
       },
     ],
-    codeSandboxReact: "",
+    codeSandboxReact: "https://codesandbox.io/s/sleepy-grass-9080ed?file=/src",
+    githubReact:
+      "https://github.com/Qiangming-L/Extensible-Components/tree/main/components/DatePicker/react",
     codeSandboxVue: "",
+    githubVue: "",
     special: `dateSegmentSelection的优先级最高,monthSegmentSelection次之,yearSegmentSelection最低
       <br/>
-      日期的范围选择起始日期的00:00:00,终点日期的23:59:59
+      日期的范围选择起始日期的00:00:00,终点日期前一天的23:59:59
       `,
     chilren: [
       {
         title: "DatePicker => DatePickerTitle",
         text: "日期前进/后退一个单位",
+        reference:
+          "引用 <a class='content-reference' target='view_window' href='https://github.com/Qiangming-L/Extensible-Components/blob/main/mixins/timestamp.tsx'>mixins/timestamp</a>",
         tbody: [
           {
             propsName: "popupPlate",
@@ -197,10 +219,17 @@ const dataArr = {
             default: "当月",
           },
         ],
+        codeSandboxReact: "",
+        githubReact:
+          "https://github.com/Qiangming-L/Extensible-Components/tree/main/components/DatePicker/react/datePickerTitle",
+        codeSandboxVue: "",
+        githubVue: "",
       },
       {
         title: "DatePicker => DatePopuop",
         text: "日期选择器的弹出层",
+        reference:
+          "引用 <a class='content-reference' target='view_window' href='https://github.com/Qiangming-L/Extensible-Components/blob/main/mixins/timestamp.tsx'>mixins/timestamp</a>",
         tbody: [
           {
             propsName: "[monthText]",
@@ -257,10 +286,17 @@ const dataArr = {
             default: "true",
           },
         ],
+        codeSandboxReact: "",
+        githubReact:
+          "https://github.com/Qiangming-L/Extensible-Components/tree/main/components/DatePicker/react/date",
+        codeSandboxVue: "",
+        githubVue: "",
       },
       {
         title: "DatePicker => DatePopuop => DateMemo",
         text: "日期选择器渲染组件",
+        reference:
+          "引用 <a class='content-reference' target='view_window' href='https://github.com/Qiangming-L/Extensible-Components/blob/main/mixins/timestamp.tsx'>mixins/timestamp</a>",
         tbody: [
           {
             propsName: "dataArr",
@@ -299,24 +335,427 @@ const dataArr = {
             default: "true",
           },
         ],
+        codeSandboxReact: "",
+        githubReact:
+          "https://github.com/Qiangming-L/Extensible-Components/blob/main/components/DatePicker/react/date/dateMemo.tsx",
+        codeSandboxVue: "",
+        githubVue: "",
         special: "dataArr 中的 which 用作 className",
+      },
+    ],
+  },
+  LazyLoad: {
+    title: "LazyLoad",
+    text: "图片的懒加载",
+    reference:
+      "引用 <a class='content-reference' target='view_window' href='https://github.com/Qiangming-L/Extensible-Components/tree/main/components/LazyLoad/img'>LazyLoad/img/default.gif</a>",
+    tbody: [
+      {
+        propsName: "data",
+        instructions: "图片所需信息",
+        type: "{url: string, imgWidth?: number | string, imgHeight?: number | string, showImg?: boolean, number?: number}",
+        default: "-",
+      },
+      {
+        propsName: "[defaultImg]",
+        instructions: "加载图片",
+        type: "string",
+        default: "default.gif",
+      },
+      {
+        propsName: "[className]",
+        instructions: "自定义class",
+        type: "string",
+        default: "-",
+      },
+      {
+        propsName: "[children]",
+        instructions: "子元素",
+        type: "HTML",
+        default: "-",
+      },
+    ],
+    codeSandboxReact:
+      "https://codesandbox.io/s/currying-waterfall-5gtily?file=/src",
+    githubReact:
+      "https://github.com/Qiangming-L/Extensible-Components/tree/main/components/LazyLoad/react",
+    codeSandboxVue: "",
+    githubVue: "",
+    special:
+      "组件使用 <a class='link' target='view_window' href='https://www.w3.org/TR/intersection-observer/' >IntersectionObserver</a> <br/>data 中的 showImg 用于是否启用图片懒加载, number 用于记录该图片的 index",
+  },
+  MagnifyingGlass: {
+    title: "MagnifyingGlass",
+    text: "图片放大镜效果",
+    tbody: [
+      {
+        propsName: "url",
+        instructions: "图片src",
+        type: "string",
+        default: "-",
+      },
+      {
+        propsName: "[children]",
+        instructions: "子元素",
+        type: "HTML",
+        default: "-",
+      },
+      {
+        propsName: "[className]",
+        instructions: "自定义组件calss",
+        type: "string",
+        default: "-",
+      },
+      {
+        propsName: "[normalStyle]",
+        instructions: "图片宽高",
+        type: "{width?: number, height?: number}",
+        default: "{width:600}",
+      },
+      {
+        propsName: "[maskLayerStyle]",
+        instructions: "图片线上范围遮罩层",
+        type: "{width?: number, height?: number}",
+        default: "{width:200}",
+      },
+      {
+        propsName: "[multiple]",
+        instructions: "放大镜效果图为遮罩层倍数",
+        type: "number",
+        default: "2",
+      },
+      {
+        propsName: "[mouseEvent]",
+        instructions: "鼠标事件",
+        type: "(type, event) => {}",
+        default: "-",
+      },
+    ],
+    codeSandboxReact: "https://codesandbox.io/s/old-currying-nl1n3n?file=/src",
+    githubReact:
+      "https://github.com/Qiangming-L/Extensible-Components/tree/main/components/MagnifyingGlass/react",
+    codeSandboxVue: "",
+    githubVue: "",
+  },
+  MagnifyingView: {
+    title: "MagnifyingView",
+    text: "图片查看",
+    tbody: [
+      {
+        propsName: "children",
+        instructions: "子元素",
+        type: "HTML",
+        default: "-",
+      },
+      {
+        propsName: "[isImmediately]",
+        instructions: "是否直接使用查看状态",
+        type: "boolean",
+        default: "false",
+      },
+      {
+        propsName: "[className]",
+        instructions: "自定义组件class",
+        type: "string",
+        default: "-",
+      },
+      {
+        propsName: "[childClassName]",
+        instructions: "自定义子元素class",
+        type: "string",
+        default: "-",
+      },
+      {
+        propsName: "[style]",
+        instructions: "自定义查看器样式",
+        type: "{width?: number | string, height?: number | string, top?: number | string, left?: number | string, backgroundColor?: string,}",
+        default: "-",
+      },
+      {
+        propsName: "[time]",
+        instructions: "图片查看动画",
+        type: "number",
+        default: "500",
+      },
+      {
+        propsName: "[magnifyingEndStyle]",
+        instructions: "自定义图片查看时 遮罩层 css 样式",
+        type: "{width: number | string = '100%', height?: number | string = '100%', top?: number | string =' 0, left?: number | string = 0', backgroundColor?: string = 'rgba(0, 0, 0, 0.5)',}",
+        default: "-",
+      },
+      {
+        propsName: "[parentOnClick]",
+        instructions: "包裹层点击事件",
+        type: "() => {}",
+        default: "-",
+      },
+    ],
+    codeSandboxReact: "https://codesandbox.io/s/misty-voice-vw7hrp?file=/src",
+    githubReact:
+      "https://github.com/Qiangming-L/Extensible-Components/blob/main/components/MagnifyingView/react",
+    codeSandboxVue: "",
+    githubVue: "",
+    special: "",
+  },
+  MasonryLayouts: {
+    title: "MasonryLayouts",
+    text: "瀑布流图片显示",
+    chilren: [
+      {
+        title: "Equal-Height",
+        text: "瀑布流-等高",
+        reference:
+          "引用 <a class='content-reference' target='view_window' href='https://github.com/Qiangming-L/Extensible-Components/tree/main/components/LazyLoad/react'>components/LazyLoad</a><a class='content-reference' target='view_window' href='https://github.com/Qiangming-L/Extensible-Components/blob/main/mixins/antiShake.tsx'>mixins/antiShake</a>",
+        tbody: [
+          {
+            propsName: "masonryLayoutsArray",
+            instructions: "渲染数据",
+            type: "[{url: string}]",
+            default: "-",
+          },
+          {
+            propsName: "[masonryLayoutsStyle]",
+            instructions: "自定义 css",
+            type: "CSSStyle",
+            default: "-",
+          },
+          {
+            propsName: "[minDifference]",
+            instructions: "每一列图片总宽与屏幕最小差",
+            type: "number",
+            default: "50",
+          },
+          {
+            propsName: "[minWidth]",
+            instructions: "容器最小宽度",
+            type: "number",
+            default: "300",
+          },
+          {
+            propsName: "[className]",
+            instructions: "自定义组件 class",
+            type: "string",
+            default: "-",
+          },
+        ],
+        codeSandboxReact:
+          "https://codesandbox.io/s/green-microservice-r33hpb?file=/src",
+        githubReact:
+          "https://github.com/Qiangming-L/Extensible-Components/blob/main/components/MasonryLayouts/Equal-Height/Horizontal-Arrangement/react",
+        codeSandboxVue: "",
+        githubVue: "",
+      },
+      {
+        title: "Equal-Width",
+        text: "瀑布流-等宽",
+        reference:
+          "引用 <a target='view_window' class='content-reference' href='https://github.com/Qiangming-L/Extensible-Components/tree/main/components/LazyLoad/react'>components/LazyLoad</a><a target='view_window' class='content-reference' href='https://github.com/Qiangming-L/Extensible-Components/blob/main/mixins/antiShake.tsx'>mixins/antiShake</a>",
+        tbody: [
+          {
+            propsName: "masonryLayoutsArray",
+            instructions: "渲染数据",
+            type: "[{url: string}]",
+            default: "-",
+          },
+          {
+            propsName: "[masonryLayoutsStyle]",
+            instructions: "自定义 css",
+            type: "CSSStyle",
+            default: "-",
+          },
+          {
+            propsName: "[minWidth]",
+            instructions: "图片最小宽度",
+            type: "number",
+            default: "300",
+          },
+          {
+            propsName: "[className]",
+            instructions: "自定义组件 class",
+            type: "string",
+            default: "-",
+          },
+        ],
+        codeSandboxReact:
+          "https://codesandbox.io/s/exciting-voice-vwwxy1?file=/src/",
+        githubReact:
+          "https://github.com/Qiangming-L/Extensible-Components/blob/main/components/MasonryLayouts/Equal-Width/Horizontal-Arrangement/react",
+        codeSandboxVue: "",
+        githubVue: "",
+        special: "",
+      },
+    ],
+  },
+  TimePicker: {
+    title: "TimePicker",
+    text: "时间选择器",
+    reference: "",
+    tbody: [
+      {
+        propsName: "[className]",
+        instructions: "自定义组件 class",
+        type: "string",
+        default: "-",
+      },
+      {
+        propsName: "[classNamePopup]",
+        instructions: "自定义弹出层组件 class",
+        type: "string",
+        default: "-",
+      },
+      {
+        propsName: "[placeholder]",
+        instructions: "输入框内提示文字",
+        type: "string",
+        default: "请选择时间",
+      },
+      {
+        propsName: "[showClearButton]",
+        instructions: "是否启用选择框中的清除按钮",
+        type: "boolean",
+        default: "true",
+      },
+      {
+        propsName: "[optionHeight]",
+        instructions: "时间选项元素高度",
+        type: "number",
+        default: "30",
+      },
+      {
+        propsName: "[amount]",
+        instructions: "显示多少列(只能是单数)",
+        type: "number",
+        default: "11",
+      },
+      {
+        propsName: "[defaultTime]",
+        instructions: "默认时间",
+        type: "string",
+        default: "00:00:00",
+      },
+    ],
+    codeSandboxReact: "https://codesandbox.io/s/awesome-noyce-w5dhem?file=/src",
+    githubReact:
+      "https://github.com/Qiangming-L/Extensible-Components/blob/main/components/TimePicker/react",
+    codeSandboxVue: "",
+    githubVue: "",
+    chilren: [
+      {
+        title: "TimePicker => TimePopup",
+        text: "时间选择器逻辑组件",
+        reference:
+          "引用 <a class='content-reference' target='view_window' href='https://github.com/Qiangming-L/Extensible-Components/blob/main/mixins/antiShake.tsx'>mixins/antiShake</a>",
+        tbody: [
+          {
+            propsName: "maxTime",
+            instructions: "最大显示时间",
+            type: "number",
+            default: "-",
+          },
+          {
+            propsName: "[moduleName]",
+            instructions: "当前模块名",
+            type: "hours | minutes | seconds",
+            default: "-",
+          },
+          {
+            propsName: "[optionHeight]",
+            instructions: "时间选项元素高度",
+            type: "number",
+            default: "30",
+          },
+          {
+            propsName: "[amount]",
+            instructions: "显示多少列(只能是单数)",
+            type: "number",
+            default: "11",
+          },
+          {
+            propsName: "[defaultTime]",
+            instructions: "默认时间",
+            type: "number",
+            default: "0",
+          },
+          {
+            propsName: "[selectedTimeFun]",
+            instructions: "选择时间后触发事件",
+            type: "(time, moduleName) => {}",
+            default: "-",
+          },
+        ],
+        codeSandboxReact: "",
+        githubReact:
+          "https://github.com/Qiangming-L/Extensible-Components/blob/main/components/TimePicker/react/time",
+        codeSandboxVue: "",
+        githubVue: "",
+      },
+      {
+        title: "TimePicker => TimeMemo",
+        text: "时间选择器渲染组件",
+        tbody: [
+          {
+            propsName: "showArr",
+            instructions: "时间渲染数据",
+            type: "[{value: string | number, key?: string | number}]",
+            default: "-",
+          },
+          {
+            propsName: "currentTime",
+            instructions: "选中的时间",
+            type: "number | string",
+            default: "-",
+          },
+          {
+            propsName: "[optionHeight]",
+            instructions: "时间选项元素高度",
+            type: "number",
+            default: "30",
+          },
+          {
+            propsName: "[amount]",
+            instructions: "显示多少列(只能是单数)",
+            type: "number",
+            default: "11",
+          },
+          {
+            propsName: "[style]",
+            instructions: "自定义组件样式",
+            type: "CSSStyle",
+            default: "-",
+          },
+          {
+            propsName: "[onScroll]",
+            instructions: "鼠标滚轮事件",
+            type: "(event) => {}",
+            default: "-",
+          },
+        ],
+        codeSandboxReact: "",
+        githubReact:
+          "https://github.com/Qiangming-L/Extensible-Components/blob/main/components/TimePicker/react/time/timeMemo.tsx",
+        codeSandboxVue: "",
+        githubVue: "",
+        special: "",
       },
     ],
   },
 };
 
 // const text = {
-//   title: "ButtonAnimation",
-//   text: "一个按钮点击小特效",
+//   title: "",
+//   text: "",
+//   reference: "",
 //   tbody: [
 //     {
-//       propsName: "children",
-//       instructions: "子元素",
-//       type: "ReactNode",
+//       propsName: "",
+//       instructions: "",
+//       type: "",
 //       default: "-",
 //     },
 //   ],
-//   codeSandboxReact:
-//     "https://codesandbox.io/s/stoic-surf-08qnte?file=/src/App.tsx",
+//   codeSandboxReact: "",
+//   githubReact: "",
 //   codeSandboxVue: "",
+//   githubVue: "",
+//   special: "",
 // };
--- index.js
+++ index.js
@@ -38,9 +38,12 @@ function updateText(_arr = dataArr[_name], _dom = "") {
   const {
     title,
     text,
+    reference,
     tbody,
     codeSandboxReact,
+    githubReact,
     codeSandboxVue,
+    githubVue,
     special,
     chilren,
   } = _arr;
@@ -50,26 +53,28 @@ function updateText(_arr = dataArr[_name], _dom = "") {
   <h3 class="content-title">${title}</h3>
     <div class="content-body">
       <p class="content-text">${text}</p>
-      <h4 class="content-props">Props</h4>
-      <table class="content-table">
-        <thead>
-          <tr>
-            <th>参数</th>
-            <th>说明</th>
-            <th>类型</th>
-            <th>默认值</th>
-          </tr>
-        </thead>
-        <tbody>
-          ${tbodyFun(tbody)}
-        </tbody>
-      </table>
+      ${reference ? referenceFun(reference) : ""}
+      ${tbody ? tbodyFun(tbody) : ""}
     `;
-  if (codeSandboxReact) {
-    _element += codeSandboxFun(codeSandboxReact);
+  if (codeSandboxReact || githubReact) {
+    _element += `<div class="body-codeSandbox">
+    <span>React:</span>
+    ${
+      codeSandboxReact
+        ? codeSandboxFun(codeSandboxReact, "react", "codeSandbox")
+        : ""
+    }
+    ${githubReact ? codeSandboxFun(githubReact) : ""}
+    </div>`;
   }
-  if (codeSandboxVue) {
-    _element += codeSandboxFun(codeSandboxReact, "vue");
+  if (codeSandboxVue || githubVue) {
+    _element += `<div class="body-codeSandbox">
+    <span>Vue:</span>
+    ${
+      codeSandboxVue ? codeSandboxFun(codeSandboxVue, "vue", "codeSandbox") : ""
+    }
+    ${githubVue ? codeSandboxFun(githubVue, "vue") : ""}
+    </div>`;
   }
   if (special) {
     _element += `<p class="special-text">${special}</p>`;
@@ -84,9 +89,28 @@ function updateText(_arr = dataArr[_name], _dom = "") {
   return _element;
 }
 
+function referenceFun(data) {
+  const _element = `
+    <div class="special-text">${data}</div>
+  `;
+  return _element;
+}
+
 function tbodyFun(tbody) {
   const Length = tbody.length;
-  let _element = "";
+  let _element = `
+    <h4 class="content-props">Props</h4>
+    <table class="content-table">
+      <thead>
+        <tr>
+          <th>参数</th>
+          <th>说明</th>
+          <th>类型</th>
+          <th>默认值</th>
+        </tr>
+      </thead>
+    <tbody>
+    `;
   for (let i = 0; i < Length; i++) {
     _element += `<tr>
       <td>${tbody[i].propsName}</td>
@@ -95,13 +119,16 @@ function tbodyFun(tbody) {
       <td>${tbody[i].default}</td>
     </tr>`;
   }
+  _element += "</tbody></table>";
   return _element;
 }
 
-function codeSandboxFun(url, type = "react") {
+function codeSandboxFun(url, type = "react", website = "github") {
   const _element = `
-    <div class="body-codeSandbox">
        <div class="body-codeSandbox-content">
+            ${
+              website === "codeSandbox"
+                ? `
             <svg
               t="1646549870371"
               class="icon codeSandbox-svg"
@@ -118,18 +145,18 @@ function codeSandboxFun(url, type = "react") {
                 d="M709.6 210l0.4-0.2h0.2L512 96 313.9 209.8h-0.2l0.7 0.3L151.5 304v416L512 928l360.5-208V304l-162.9-94zM482.7 843.6L339.6 761V621.4L210 547.8V372.9l272.7 157.3v313.4zM238.2 321.5l134.7-77.8 138.9 79.7 139.1-79.9 135.2 78-273.9 158-274-158zM814 548.3l-128.8 73.1v139.1l-143.9 83V530.4L814 373.1v175.2z"
                 p-id="1546"
               ></path>
-            </svg>
+            </svg>`
+                : `
+            <svg t="1646630344710" class="icon codeSandbox-svg" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2013" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><style type="text/css"></style></defs><path d="M512 42.666667A464.64 464.64 0 0 0 42.666667 502.186667 460.373333 460.373333 0 0 0 363.52 938.666667c23.466667 4.266667 32-9.813333 32-22.186667v-78.08c-130.56 27.733333-158.293333-61.44-158.293333-61.44a122.026667 122.026667 0 0 0-52.053334-67.413333c-42.666667-28.16 3.413333-27.733333 3.413334-27.733334a98.56 98.56 0 0 1 71.68 47.36 101.12 101.12 0 0 0 136.533333 37.973334 99.413333 99.413333 0 0 1 29.866667-61.44c-104.106667-11.52-213.333333-50.773333-213.333334-226.986667a177.066667 177.066667 0 0 1 47.36-124.16 161.28 161.28 0 0 1 4.693334-121.173333s39.68-12.373333 128 46.933333a455.68 455.68 0 0 1 234.666666 0c89.6-59.306667 128-46.933333 128-46.933333a161.28 161.28 0 0 1 4.693334 121.173333A177.066667 177.066667 0 0 1 810.666667 477.866667c0 176.64-110.08 215.466667-213.333334 226.986666a106.666667 106.666667 0 0 1 32 85.333334v125.866666c0 14.933333 8.533333 26.88 32 22.186667A460.8 460.8 0 0 0 981.333333 502.186667 464.64 464.64 0 0 0 512 42.666667" p-id="2014"></path></svg>
+            `
+            }
             <a
               href="${url}"
               target="view_window"
               class="codeSandbox-link"
               id="codeSandboxTLinkReact"
             ></a>
-            <div class="codeSandbox-popup triangle">${
-              type === "react" ? "CodeSandbox-React" : "CodeSandbox-Vue"
-            }</div>
         </div>
-    </div>
     `;
   return _element;
 }
